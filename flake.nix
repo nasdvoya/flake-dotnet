867e9ec7604e40319d1216a58f981f2d960e7709
sasha <nasdvoya@proton.me>
2024-07-29 09:46:06 +0100

    Rollback

--- flake.nix
+++ flake.nix
@@ -14,25 +14,8 @@
       devShells = forEachSupportedSystem ({ pkgs }: {
         default = pkgs.mkShell {
           packages = with pkgs; [
-            (with dotnetCorePackages; combinePackages [ sdk_6_0 sdk_7_0 sdk_8_0 ])
-            (buildDotnetGlobalTool {
-              pname = "Nuke.GlobalTool";
-              version = "6.2.1"; # Update to the desired version
-              nugetName = "Nuke.GlobalTool";
-              nugetHash = "sha256-<hash>"; # Replace <hash> with the actual hash obtained after the first build
-              meta = {
-                description = "Nuke build automation tool for C# and .NET.";
-                homepage = "https://nuke.build/";
-                license = pkgs.lib.licenses.mit;
-                platforms = pkgs.lib.platforms.all;
-              };
-            })
+            (with dotnetCorePackages; combinePackages [ sdk_6_0 sdk_7_0 sdk_8_0  ])
           ];
-
-          shellHook = ''
-            export DOTNET_ROOT=$(dirname $(realpath $(which dotnet)))
-            export PATH="$HOME/.dotnet/tools:$PATH"
-          '';
         };
       });
     };
